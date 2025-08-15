7866f5a1a6cffb0851b07a6044ec3e0277b3069f
chanisiree <674259005@webmail.npru.ac.th>
2025-08-15 16:32:57 +0700

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a = 10,b=5;
+        int sum = a + b ;
+        int difference = a - b;
+        int multiply = a * b;
+        int div = a / b ;
+        int mod = a % b;
+        System.out.println("ผลบวก : "+ sum);
+        System.out.println("ผลลบ : "+ difference);
+        System.out.println("ผลคูณ : "+ multiply);
+        System.out.println("ผลหาร : "+ div);
+        System.out.println("เศษที่เหลือ : "+mod);
+    }
+    }
---
+++ BMICalculator.java
@@ -0,0 +1,5 @@
+public class BMICalculator {
+    int weight = 50 kg;
+    int height = 1.64 meters;
+    
+}
